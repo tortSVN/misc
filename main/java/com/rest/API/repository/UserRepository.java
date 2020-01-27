a2429236cd8480dd6078f45b2deacd5914aae670
svn <sonvnguy@gmail.com>
2020-01-26 22:00:03 -0500

    misc. java project 1st commit

--- main/java/com/rest/API/repository/UserRepository.java
+++ main/java/com/rest/API/repository/UserRepository.java
@@ -5,9 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-//	@Query(value="Select * from users", nativeQuery=true)
-//	List<Contact> getAllContacts();
-//	
-//	@Query(value="Select * from contacts s where (:period is null OR to_char(s.timedatestamp, 'yyyy-MM-dd') <= :period)", nativeQuery=true)
-//	List<Contact> getSomeContacts(@Param("period") String period);
+	@Query(value="Select * from users", nativeQuery=true)
+	List<Contact> getAllContacts();
+	
+	@Query(value="Select * from contacts s where (:period is null OR to_char(s.timedatestamp, 'yyyy-MM-dd') <= :period)", nativeQuery=true)
+	List<Contact> getSomeContacts(@Param("period") String period);
 }
